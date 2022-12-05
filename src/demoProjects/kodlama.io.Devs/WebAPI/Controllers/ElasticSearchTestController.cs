e8b1672d9e7368db2b5e9452476587f610dd0a9c
ashwor11 <75903644+ashwor11@users.noreply.github.com>
2022-12-06 01:23:58 +0300

    elasticsearch basicly implemented.

--- src/demoProjects/kodlama.io.Devs/WebAPI/Controllers/ElasticSearchTestController.cs
+++ src/demoProjects/kodlama.io.Devs/WebAPI/Controllers/ElasticSearchTestController.cs
@@ -1,5 +1,5 @@
 ﻿using Application.Features.ProgrammingLanguages.Commands.CreateProgrammingLanguage;
-using Application.Features.ProgrammingLanguages.Models;
+using Application.Features.ProgrammingLanguages.Dtos;
 using Core.ElasticSearch;
 using Core.ElasticSearch.Models;
 using Microsoft.AspNetCore.Http;
@@ -10,48 +10,65 @@ namespace WebAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ElasticTestController : BaseController
+    public class ElasticSearchTestController : BaseController
     {
         private readonly IElasticSearch _elasticSearch;
 
-        public ElasticTestController(IElasticSearch elasticSearch)
+        public ElasticSearchTestController(IElasticSearch elasticSearch)
         {
             _elasticSearch = elasticSearch;
         }
+
 
         [HttpGet]
         public async Task<IActionResult> Get()
         {
             IElasticSearchResult result = await _elasticSearch.CreateNewIndexAsync(new IndexModel
             {
-                IndexName = "models",
-                AliasName = "amodels",
+                IndexName = "programminglanguages",
+                AliasName = "aprogramminglanguages",
                 NumberOfReplicas = 1,
                 NumberOfShards = 1
             });
 
+            
+
             ElasticSearchInsertUpdateModel model = new()
             {
-                IndexName = "models",
+                IndexName = "programminglanguages",
                 Item = new CreateProgrammingLanguageCommand
-                { Name = "Fortran" }
+                { Name = "C" }
+            };
+            ElasticSearchInsertUpdateModel model1 = new()
+            {
+                IndexName = "programminglanguages",
+                Item = new CreateProgrammingLanguageCommand
+                { Name = "C++" }
+            };
+            ElasticSearchInsertUpdateModel model2 = new()
+            {
+                IndexName = "programminglanguages",
+                Item = new CreateProgrammingLanguageCommand
+                { Name = "C#" }
             };
 
             IElasticSearchResult result2 = await _elasticSearch.InsertAsync(model);
+            IElasticSearchResult result2X = await _elasticSearch.InsertAsync(model2);
+            IElasticSearchResult result2XD = await _elasticSearch.InsertAsync(model1);
+
 
             IEnumerable<IndexName> result3 = _elasticSearch.GetIndexList().Keys;
 
-            List<ElasticSearchGetModel<ProgrammingLanguageListModel>> result4 = await
-                                                                    _elasticSearch.GetSearchByField<ProgrammingLanguageListModel>(
+            List<ElasticSearchGetModel<ProgrammingLanguageListDto>> result4 = await
+                                                                    _elasticSearch.GetSearchByField<ProgrammingLanguageListDto>(
                                                                         new SearchByFieldParameters
                                                                         {
                                                                             IndexName = "programminglanguages",
                                                                             FieldName = "Name",
-                                                                            Value = "C#"
+                                                                            Value = "C"
                                                                         });
 
             return Ok(result4);
         }
-
     }
 }
